6b6c75aea7839920cad91c2ab32607a91cec6f17
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2025-05-04 16:27:58 -0400

    imporve tests with test tables for create and delete entryapi

--- api/services/pwmanager/tests/entryapi/create_test.go
+++ api/services/pwmanager/tests/entryapi/create_test.go
@@ -75,7 +75,7 @@ func create200(sd apitest.SeedData) []apitest.Table {
 func create400(sd apitest.SeedData) []apitest.Table {
 	table := []apitest.Table{
 		{
-			Name:       "missing-input",
+			Name:       fmt.Sprintf("tu%d-missing-input", userBundleAdmin),
 			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
 			Token:      sd.Users[userBundleAdmin].Token,
 			Method:     http.MethodPost,
@@ -95,106 +95,90 @@ func create400(sd apitest.SeedData) []apitest.Table {
 }
 
 func create401(sd apitest.SeedData) []apitest.Table {
-	table := []apitest.Table{
+	table := []apitest.Table{}
+	inputs := []struct {
+		name  string
+		token string
+		err   *errs.Error
+	}{
 		{
-			Name:       "emptytoken",
+			"emptytoken",
+			"&nbsp;",
+			errs.Newf(errs.Unauthenticated, "error parsing token: token contains an invalid number of segments"),
+		},
+		{
+			"badtoken",
+			sd.Users[userBundleAdmin].Token[:10],
+			errs.Newf(errs.Unauthenticated, "error parsing token: token contains an invalid number of segments"),
+		},
+		{
+			"badsig",
+			sd.Users[userBundleAdmin].Token + "A",
+			errs.Newf(errs.Unauthenticated, "authentication failed : bindings results[[{[true] map[x:false]}]] ok[true]"),
+		},
+	}
+
+	for _, i := range inputs {
+		t := apitest.Table{
+			Name:       fmt.Sprintf("tu%d-%s", userBundleAdmin, i.name),
 			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
-			Token:      "&nbsp;",
+			Token:      i.token,
 			Method:     http.MethodPost,
 			StatusCode: http.StatusUnauthorized,
 			GotResp:    &errs.Error{},
-			ExpResp:    errs.Newf(errs.Unauthenticated, "error parsing token: token contains an invalid number of segments"),
+			ExpResp:    i.err,
 			CmpFunc: func(got any, exp any) string {
 				return cmp.Diff(got, exp)
 			},
-		},
-		{
-			Name:       "badtoken",
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
-			Token:      sd.Users[userBundleAdmin].Token[:10],
-			Method:     http.MethodPost,
-			StatusCode: http.StatusUnauthorized,
-			GotResp:    &errs.Error{},
-			ExpResp:    errs.Newf(errs.Unauthenticated, "error parsing token: token contains an invalid number of segments"),
-			CmpFunc: func(got any, exp any) string {
-				return cmp.Diff(got, exp)
-			},
-		},
-		{
-			Name:       "badsig",
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
-			Token:      sd.Users[userBundleAdmin].Token + "A",
-			Method:     http.MethodPost,
-			StatusCode: http.StatusUnauthorized,
-			GotResp:    &errs.Error{},
-			ExpResp:    errs.Newf(errs.Unauthenticated, "authentication failed : bindings results[[{[true] map[x:false]}]] ok[true]"),
-			CmpFunc: func(got any, exp any) string {
-				return cmp.Diff(got, exp)
-			},
-		},
+		}
+
+		table = append(table, t)
 	}
 
 	return table
 }
 
 func create403(sd apitest.SeedData) []apitest.Table {
-	table := []apitest.Table{
+	inputs := []struct {
+		user       userKey
+		errMessage string
+	}{
 		{
-			Name:       fmt.Sprintf("tu%d-shared-read-only", userRead),
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
-			Token:      sd.Users[userRead].Token,
-			Method:     http.MethodPost,
-			StatusCode: http.StatusForbidden,
-			Input: &entryapp.NewEntryTX{
-				Data:     "Guitar",
-				Metadata: "UPDATED BUNDLE METADATA",
-			},
-			GotResp: &errs.Error{},
-			ExpResp: errs.Newf(errs.PermissionDenied, ""),
-			CmpFunc: func(got any, exp any) string {
-				// &errs.Error{Code:errs.ErrCode{value:8}, Message:"must have write perms for bundle[bb459b0a-79a5-4af9-b89a-67d830ec1db9] to create an entry", FuncName:"", FileName:""}
-				expResp := exp.(*errs.Error)
-				expResp.Message = fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID)
-				return cmp.Diff(got, exp)
-			},
+			userRead,
+			fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID),
 		},
 		{
-			Name:       fmt.Sprintf("tu%d-shared-no-roles", userNoRoles),
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
-			Token:      sd.Users[userNoRoles].Token,
-			Method:     http.MethodPost,
-			StatusCode: http.StatusForbidden,
-			Input: &entryapp.NewEntryTX{
-				Data:     "Guitar",
-				Metadata: "UPDATED BUNDLE METADATA",
-			},
-			GotResp: &errs.Error{},
-			ExpResp: errs.Newf(errs.PermissionDenied, ""),
-			CmpFunc: func(got any, exp any) string {
-				expResp := exp.(*errs.Error)
-				expResp.Message = fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID)
-				return cmp.Diff(got, exp)
-			},
+			userNoRoles,
+			fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID),
 		},
 		{
-			Name:       fmt.Sprintf("tu%d-shared-no-key", userNoKey),
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
-			Token:      sd.Users[userNoKey].Token,
-			Method:     http.MethodPost,
-			StatusCode: http.StatusForbidden,
-			Input: &entryapp.NewEntryTX{
-				Data:     "Guitar",
-				Metadata: "UPDATED BUNDLE METADATA",
-			},
-			GotResp: &errs.Error{},
-			ExpResp: errs.Newf(errs.PermissionDenied, ""),
-			CmpFunc: func(got any, exp any) string {
-				expResp := exp.(*errs.Error)
+			userNoKey,
+			fmt.Sprintf("query: userID[%s] bundleID[%s]: db: key not found", sd.Users[userNoKey].ID, sd.Users[userBundleAdmin].Bundles[0].ID),
+		},
+	}
 
-				expResp.Message = fmt.Sprintf("query: userID[%s] bundleID[%s]: db: key not found", sd.Users[userNoKey].ID, sd.Users[userBundleAdmin].Bundles[0].ID)
+	table := []apitest.Table{}
+	for _, i := range inputs {
+		t := apitest.Table{
+			Name:       fmt.Sprintf("tu%d-%s", userRead, userKeyMapping[i.user]),
+			URL:        fmt.Sprintf("/v1/bundles/%s/entries", sd.Users[userBundleAdmin].Bundles[0].ID.String()),
+			Token:      sd.Users[i.user].Token,
+			Method:     http.MethodPost,
+			StatusCode: http.StatusForbidden,
+			Input: &entryapp.NewEntryTX{
+				Data:     "Guitar",
+				Metadata: "UPDATED BUNDLE METADATA",
+			},
+			GotResp: &errs.Error{},
+			ExpResp: errs.Newf(errs.PermissionDenied, ""),
+			CmpFunc: func(got any, exp any) string {
+				expResp := exp.(*errs.Error)
+				expResp.Message = i.errMessage
 				return cmp.Diff(got, exp)
 			},
-		},
+		}
+
+		table = append(table, t)
 	}
 	return table
 }
--- api/services/pwmanager/tests/entryapi/delete_test.go
+++ api/services/pwmanager/tests/entryapi/delete_test.go
@@ -11,13 +11,25 @@ import (
 )
 
 func delete200(sd apitest.SeedData) []apitest.Table {
-	table := []apitest.Table{
+	table := []apitest.Table{}
+	inputs := []struct {
+		user userKey
+	}{
 		{
-			Name:  fmt.Sprintf("tu%d-user-bundle-admin", userBundleAdmin),
-			URL:   fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[0].ID, sd.Users[userBundleAdmin].Entries[0].ID),
-			Token: sd.Users[userBundleAdmin].Token,
+			userBundleAdmin,
+		},
+		{
+			userReadWrite,
+		},
+	}
+
+	for idx, i := range inputs {
+		t := apitest.Table{
+			Name:  fmt.Sprintf("tu%d-%s", i.user, userKeyMapping[i.user]),
+			URL:   fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[0].ID, sd.Users[userBundleAdmin].Entries[idx].ID),
+			Token: sd.Users[i.user].Token,
 			Input: &entryapp.DeleteEntry{
-				Metadata: "UPDATED BUNDLE METADATA",
+				Metadata: fmt.Sprintf("UPDATED BUNDLE METADATA %d", i.user),
 			},
 			Method:     http.MethodDelete,
 			StatusCode: http.StatusOK,
@@ -26,117 +38,103 @@ func delete200(sd apitest.SeedData) []apitest.Table {
 			CmpFunc: func(got any, exp any) string {
 				gotResp := got.(*entryapp.EntryTx)
 				gotMetadata := gotResp.Bundle.Metadata
-				expMetadata := "UPDATED BUNDLE METADATA"
+				expMetadata := fmt.Sprintf("UPDATED BUNDLE METADATA %d", i.user)
 				return cmp.Diff(gotMetadata, expMetadata)
 			},
-		},
-		{
-			Name:  fmt.Sprintf("tu%d-shared-read-write", userReadWrite),
-			URL:   fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[0].ID, sd.Users[userBundleAdmin].Entries[1].ID),
-			Token: sd.Users[userReadWrite].Token,
-			Input: &entryapp.DeleteEntry{
-				Metadata: "UPDATED BUNDLE METADATA",
-			},
-			Method:     http.MethodDelete,
-			StatusCode: http.StatusOK,
-			GotResp:    &entryapp.EntryTx{},
-			ExpResp:    &entryapp.EntryTx{},
-			CmpFunc: func(got any, exp any) string {
-				gotResp := got.(*entryapp.EntryTx)
-				gotMetadata := gotResp.Bundle.Metadata
-				expMetadata := "UPDATED BUNDLE METADATA"
-				return cmp.Diff(gotMetadata, expMetadata)
-			},
-		},
+		}
+
+		table = append(table, t)
 	}
 
 	return table
 }
 
 func delete401(sd apitest.SeedData) []apitest.Table {
-	table := []apitest.Table{
+	table := []apitest.Table{}
+	inputs := []struct {
+		name  string
+		token string
+		err   *errs.Error
+	}{
 		{
-			Name:       "emptytoken",
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[1].ID, sd.Users[userBundleAdmin].Entries[2].ID),
-			Token:      "&nbsp;",
-			Method:     http.MethodDelete,
-			StatusCode: http.StatusUnauthorized,
-			GotResp:    &errs.Error{},
-			ExpResp:    errs.Newf(errs.Unauthenticated, "error parsing token: token contains an invalid number of segments"),
-			CmpFunc: func(got any, exp any) string {
-				return cmp.Diff(got, exp)
-			},
+			"emptytoken",
+			"&nbsp;",
+			errs.Newf(errs.Unauthenticated, "error parsing token: token contains an invalid number of segments"),
 		},
 		{
-			Name:       "badsig",
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[1].ID, sd.Users[userBundleAdmin].Entries[2].ID),
-			Token:      sd.Users[userBundleAdmin].Token + "A",
-			Method:     http.MethodDelete,
-			StatusCode: http.StatusUnauthorized,
-			GotResp:    &errs.Error{},
-			ExpResp:    errs.Newf(errs.Unauthenticated, "authentication failed : bindings results[[{[true] map[x:false]}]] ok[true]"),
-			CmpFunc: func(got any, exp any) string {
-				return cmp.Diff(got, exp)
-			},
+			"badtoken",
+			sd.Users[userBundleAdmin].Token[:10],
+			errs.Newf(errs.Unauthenticated, "error parsing token: token contains an invalid number of segments"),
+		},
+		{
+			"badsig",
+			sd.Users[userBundleAdmin].Token + "A",
+			errs.Newf(errs.Unauthenticated, "authentication failed : bindings results[[{[true] map[x:false]}]] ok[true]"),
 		},
 	}
+
+	for _, i := range inputs {
+		t := apitest.Table{
+
+			Name:       fmt.Sprintf("tu%d-%s", userBundleAdmin, i.name),
+			URL:        fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[1].ID, sd.Users[userBundleAdmin].Entries[2].ID),
+			Token:      i.token,
+			Method:     http.MethodDelete,
+			StatusCode: http.StatusUnauthorized,
+			GotResp:    &errs.Error{},
+			ExpResp:    i.err,
+			CmpFunc: func(got any, exp any) string {
+				return cmp.Diff(got, exp)
+			},
+		}
+
+		table = append(table, t)
+	}
+
 	return table
+
 }
 
 func delete403(sd apitest.SeedData) []apitest.Table {
-	table := []apitest.Table{
+	inputs := []struct {
+		user       userKey
+		errMessage string
+	}{
 		{
-			Name:       fmt.Sprintf("tu%d-shared-user-read-only", userRead),
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[0].ID, sd.Users[userBundleAdmin].Entries[2].ID),
-			Token:      sd.Users[userRead].Token,
-			Method:     http.MethodDelete,
-			StatusCode: http.StatusForbidden,
-			GotResp:    &errs.Error{},
-			Input: &entryapp.DeleteEntry{
-				Metadata: "UPDATED BUNDLE METADATA",
-			},
-			ExpResp: errs.Newf(errs.PermissionDenied, ""),
-			CmpFunc: func(got any, exp any) string {
-				expResp := exp.(*errs.Error)
-				expResp.Message = fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID)
-				return cmp.Diff(got, exp)
-			},
+			userRead,
+			fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID),
 		},
 		{
-			Name:       fmt.Sprintf("tu%d-shared-user-no-roles", userNoRoles),
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[0].ID, sd.Users[userBundleAdmin].Entries[2].ID),
-			Token:      sd.Users[userNoRoles].Token,
-			Method:     http.MethodDelete,
-			StatusCode: http.StatusForbidden,
-			GotResp:    &errs.Error{},
-			Input: &entryapp.DeleteEntry{
-				Metadata: "UPDATED BUNDLE METADATA",
-			},
-			ExpResp: errs.Newf(errs.PermissionDenied, ""),
-			CmpFunc: func(got any, exp any) string {
-				expResp := exp.(*errs.Error)
-				expResp.Message = fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID)
-				return cmp.Diff(got, exp)
-			},
+			userNoRoles,
+			fmt.Sprintf("must have write perms for bundle[%s] to create an entry", sd.Users[userBundleAdmin].Bundles[0].ID),
 		},
 		{
-			Name:       fmt.Sprintf("tu%d-shared-user-no-key", userNoKey),
-			URL:        fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[0].ID, sd.Users[userBundleAdmin].Entries[2].ID),
-			Token:      sd.Users[userNoKey].Token,
-			Method:     http.MethodDelete,
-			StatusCode: http.StatusForbidden,
-			GotResp:    &errs.Error{},
-			Input: &entryapp.DeleteEntry{
-				Metadata: "UPDATED BUNDLE METADATA",
-			},
-			ExpResp: errs.Newf(errs.PermissionDenied, ""),
-			CmpFunc: func(got any, exp any) string {
-				expResp := exp.(*errs.Error)
+			userNoKey,
+			fmt.Sprintf("query: userID[%s] bundleID[%s]: db: key not found", sd.Users[userNoKey].ID, sd.Users[userBundleAdmin].Bundles[0].ID),
+		},
+	}
 
-				expResp.Message = fmt.Sprintf("query: userID[%s] bundleID[%s]: db: key not found", sd.Users[userNoKey].ID, sd.Users[userBundleAdmin].Bundles[0].ID)
+	table := []apitest.Table{}
+	for _, i := range inputs {
+		t := apitest.Table{
+			Name:       fmt.Sprintf("tu%d-%s", i.user, userKeyMapping[i.user]),
+			URL:        fmt.Sprintf("/v1/bundles/%s/entries/%s", sd.Users[userBundleAdmin].Bundles[0].ID, sd.Users[userBundleAdmin].Entries[2].ID),
+			Token:      sd.Users[i.user].Token,
+			Method:     http.MethodDelete,
+			StatusCode: http.StatusForbidden,
+			GotResp:    &errs.Error{},
+			Input: &entryapp.DeleteEntry{
+				Metadata: "UPDATED BUNDLE METADATA",
+			},
+			ExpResp: errs.Newf(errs.PermissionDenied, ""),
+			CmpFunc: func(got any, exp any) string {
+				expResp := exp.(*errs.Error)
+				expResp.Message = i.errMessage
 				return cmp.Diff(got, exp)
 			},
-		},
+		}
+
+		table = append(table, t)
 	}
 	return table
 }
